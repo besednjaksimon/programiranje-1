4ab8078558ae22076ffa6aed930011fa2a274d6f
Simon B <besednjak.simon@gmail.com>
2019-01-14 14:07:21 +0100

    vaje_12

--- 12-dinamicno-programiranje/exercises/dynamic_programing.ml
+++ 12-dinamicno-programiranje/exercises/dynamic_programing.ml
@@ -21,6 +21,34 @@ let test_matrix =
      [| 2 ; 4 ; 5 |];
      [| 7 ; 0 ; 1 |] |]
 
+let max_cheese cheese_matrix =
+  let rec aux i j cheese_matrix =
+    let n = Array.length cheese_matrix.(0)
+    and m = Array.length cheese_matrix in
+    if (n-i) > 0 && (m-j) > 0 then
+      cheese_matrix.(i).(j) + max (aux (i+1) j cheese_matrix) (aux i (j+1) cheese_matrix)
+    else if n = i then
+      
+    else
+      Array.fold_left (+) 0 (Array.sub cheese_matrix i n)
+  in
+  aux 0 0 cheese_matrix
+
+(* 
+let max_cheese cheese_matrix =
+  let dimx = Array.length cheese_matrix in
+  if dimx = 0 then 0 else
+  let dimy = Array.lenght cheese_matrix.(0) in
+  let rec best_path x y =
+    print_endline(string_of_int x ^ ", " ^ (string_of_int y))
+    let current_cheese = cheese_matrix.(x).(y) in
+    let best_down = if (y+1) = dimy then 0 else best_path x (y+1)
+    and best_right = if (x+1) = dimx then 0 else best_path (x+1) y in
+    current_cheese + max best_right best_down
+  in
+  best_path 0 0
+*)
+
 (*----------------------------------------------------------------------------*]
  We are solving the problem of alternatingly colored towers. There are four
  different types of building blocks, two of them blue and two red. The blue
@@ -36,6 +64,18 @@ let test_matrix =
  - : int = 35
 [*----------------------------------------------------------------------------*)
 
+let rec red = function
+  | 0 -> 1
+  | n when n < 0 -> 0
+  | n -> blue (n - 1) + blue (n - 2)
+and blue = function
+  | 0 -> 1
+  | n when n < 0 -> 0
+  | n -> red (n - 2) + red (n - 3)
+
+let alternating_towers = function
+  | 0 -> 1
+  | n -> red n + blue n
 
 (*----------------------------------------------------------------------------*]
  You have won a coupon for Mercator, allowing you to purchase any articles in
@@ -67,3 +107,9 @@ let articles = [|
   ("Nutella", 4.99, 0.75);
   ("juice", 1.15, 2.0)
 |]
+
+let rec best_value articles max_w =
+  let rec get_item articles max_w price (_,apr,aw) =
+
+  
+  and shopper 

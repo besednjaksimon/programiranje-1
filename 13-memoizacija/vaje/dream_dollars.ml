3024133604462785106ab16739df1785ce0c8a11
Simon B <besednjak.simon@gmail.com>
2019-01-21 14:04:19 +0100

    Vaje_13

--- 13-memoizacija/vaje/dream_dollars.ml
+++ 13-memoizacija/vaje/dream_dollars.ml
@@ -14,14 +14,20 @@ let denominations = [1; 4; 7; 13; 28; 52; 91; 365]
 (* 0.i) Formulate the problem precisely in natural language. *)
 
 (*
-   Given an amount n, ...
+   Given an amount n, find the fewest numbers from the list denominations,
+   so that their sum equals n.
+   or
+   Find a list l such that sum(l) = n where each x € l is also € denominations.
 *)
 
 
 (* 0.ii) Describe the problem recursively. *)
 
 (*
-   Given an amount n, ...
+   Given an amount n, pick all numbers m_i from the list, which are 
+   still smaller than n, and then solve the problem for n-m_i. We also
+   have to add candidate to the list. In the end, we return the shortest
+   list.
 *)
 
 
@@ -38,14 +44,58 @@ let denominations = [1; 4; 7; 13; 28; 52; 91; 365]
    later.
 *)
 
-let rec bills_greedy n = failwith "todo"
+let find_largest_element_smaller_than x xs =
+  let rec aux acc = function
+    | [] -> acc
+    | y :: ys ->
+      if y <= x then
+        let acc' = match acc with
+        | None -> Some y
+        | Some z -> Some (max y z)
+        in
+        aux acc' ys
+      else
+        acc
+  in
+  aux None xs
+
+let bills_greedy n denominations =
+  let rec aux acc n =
+    if n = 0 then
+      acc
+    else
+      match find_largest_element_smaller_than n denominations with
+      | None -> failwith "Error"
+      | Some el -> aux (el :: acc) (n-el)
+  in
+  aux [] n
 
 (* 2.i) Describe and analyze a recursive algorithm that computes, given an
    integer k, the shortest list of bills needed to make k Dream Dollars. (Don’t
    worry about making your algorithm fast; just make sure it’s correct.)
 *)
 
-let rec bills_rec n = failwith "todo"
+let rec i_th_element i xs =
+  match i, xs with
+    | _, [] -> failwith "Error"
+    | 0, x :: _ -> x
+    | i, x :: xs -> i_th_element (i-1) xs
+
+let rec bills_rec n =
+  if n = 0 then [] else
+  let sols = List.map (fun cand -> bills_rec (n-cand) @ [cand]) (List.filter (fun x -> x <= n) denominations)
+  in let sols' = List.sort (fun s1 s2 -> compare (List.length s1) (List.length s2)) sols
+  in match sols' with
+      | [] -> failwith "Impossible"
+      | s :: _ -> s
+    
+    (* match n, candidates with
+      | 0, _ -> acc
+      | _, [] -> failwith "Error"
+      | n, x :: candidates ->
+        aux (x :: acc) (n-x) (x :: candidates) *)
+      
+      
 
 
 (* 2.ii) Draw the call tree of your recursive definition for n = 5 and identify
@@ -62,4 +112,17 @@ let rec bills_rec n = failwith "todo"
    to be fast.)
 *)
 
-let bills_iter n = failwith "todo"
+let memoiziraj f =
+  let rezultati = Hashtbl.create 512 in
+  let mem_f x =
+    match Hashtbl.find_opt rezultati x with
+    | None ->
+        let y = f x in
+        Hashtbl.add rezultati x y;
+        y
+    | Some y ->
+        y
+  in
+  mem_f
+
+let bills_iter n = memoiziraj (bills_rec n)
